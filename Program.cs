c6714412115010086367006d03d5895afb876833
nsaori <hana-chonsaoring0@yahoo.co.jp>
2016-10-21 18:39:00 +0900

    0208

--- Program.cs
+++ Program.cs
@@ -11,10 +11,13 @@ namespace ch0208
         static void Main(string[] args)
         {
             Console.Write("radiuse? ");
+
             string sradiuse = Console.ReadLine();
             int radiuse = int.Parse(sradiuse);
+
             double arc = 2 * Math.PI * radiuse;
             double w = 3.14 * radiuse * radiuse;
+
             Console.WriteLine("돌레: " + arc + "\t 넓이: " + w);
 
         }
